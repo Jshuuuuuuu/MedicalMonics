653a04d1dc1de82d3c94e2aaadf6d9b81704bcfb
franko <frankovgarcia1123@gmail.com>
2025-06-08 05:33:53 +0800

    library color change

--- medmonics/src/pages/LibraryPage.jsx
+++ medmonics/src/pages/LibraryPage.jsx
@@ -5,8 +5,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { useToast } from '../contexts/ToastContext';
 import Modal from '../contexts/Modal';
 import { getMnemonicsCollectionPath } from '../firebase';
-import '../styles/common.css';
-import '../styles/LibraryPage.css';
+import '../styles/common.css';import '../styles/LibraryPage.css';
 
 
 const LibraryPage = ({ onEditMnemonic }) => {
